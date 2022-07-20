1e018e12a267756b29296618cfbed720be6bf9f0
Musa <79163385+musayuksel@users.noreply.github.com>
2022-07-20 14:53:44 +0100

    Add send button

--- src/components/NewMessageForm.jsx
+++ src/components/NewMessageForm.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function NewMessageForm() {
-  return <input data-testid="messageText" type="text" />;
+  return (
+    <>
+      <input data-testid="messageText" type="text" />
+      <button data-testid="sendButton">Send</button>
+    </>
+  );
 }
