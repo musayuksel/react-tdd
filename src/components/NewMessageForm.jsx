d98f3da05bf41e106cfc984fbcc402eecb99eedb
Musa <79163385+musayuksel@users.noreply.github.com>
2022-07-20 15:30:52 +0100

    Add states for message

--- src/components/NewMessageForm.jsx
+++ src/components/NewMessageForm.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function NewMessageForm() {
+  const [inputMessage, setInputMessage] = useState("");
+  function handleSendButtonClick() {
+    console.log("send button clicked");
+    setInputMessage("");
+  }
   return (
     <>
-      <input data-testid="messageText" type="text" />
-      <button data-testid="sendButton">Send</button>
+      <input
+        value={inputMessage}
+        onChange={(e) => {
+          setInputMessage(e.target.value);
+        }}
+        data-testid="messageText"
+        type="text"
+      />
+      <button onClick={handleSendButtonClick} data-testid="sendButton">
+        Send
+      </button>
     </>
   );
 }
