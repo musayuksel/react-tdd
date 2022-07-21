66c60f5fb992473ad75098b671a0d29511d1edd6
Musa <79163385+musayuksel@users.noreply.github.com>
2022-07-21 14:59:57 +0100

    Show restauranst on the screen

--- src/components/RestaurantListPage.jsx
+++ src/components/RestaurantListPage.jsx
@@ -3,10 +3,18 @@ import NewRestaurantForm from "./NewRestaurantForm";
 
 export default function RestaurantListPage() {
   const [restaurants, setRestaurants] = useState([]);
+  function saveHandler(name) {
+    setRestaurants((prevRestaurants) => [...prevRestaurants, name]);
+  }
   return (
     <section>
-      <NewRestaurantForm />
-      <button data-test="addRestaurantButton">Add restaurant</button>
+      <NewRestaurantForm saveHandler={saveHandler} />
+      <button data-testid="addRestaurantButton">Add restaurant</button>
+      <ul data-testid="restaurantList">
+        {restaurants.map((restaurant) => (
+          <li key={restaurant}>{restaurant}</li>
+        ))}
+      </ul>
     </section>
   );
 }
