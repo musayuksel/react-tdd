7a4106af8429415d8f712de1d2b21117c432d818
Musa <79163385+musayuksel@users.noreply.github.com>
2022-07-21 12:03:16 +0100

    Update smoke test

--- cypress/e2e/smoke_test.cy.js
+++ cypress/e2e/smoke_test.cy.js
@@ -1,6 +1,6 @@
 describe("Creating a message", () => {
   it("Find the viewer", () => {
-    cy.visit("http://localhost:3000");
+    cy.visit("http://localhost:3000").contains("Hello Musa!!!");
 
     //   cy.get('[data-testid="messageText"]').type("New message");
 
@@ -9,6 +9,5 @@ describe("Creating a message", () => {
     //   cy.get('[data-testid="messageText"]').should("have.value", "");
 
     //   cy.contains("New message");
-    expect(true).equal(true);
   });
 });
