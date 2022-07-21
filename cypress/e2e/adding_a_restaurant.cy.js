dc700edf9df185475d0c57dd60826f14a3ada549
Musa <79163385+musayuksel@users.noreply.github.com>
2022-07-21 16:09:41 +0100

    Add e2e test for form control flow

--- cypress/e2e/adding_a_restaurant.cy.js
+++ cypress/e2e/adding_a_restaurant.cy.js
@@ -2,9 +2,13 @@ describe("adding a restaurant", () => {
   it("display the restaurant in the list", () => {
     const restaurantName = "Sushi Place";
     cy.visit("http://localhost:3000");
+    //confirm newRestaurantForm is not displayed
+    cy.get('[data-testid="newRestaurantForm"]').should("not.exist");
     cy.get('[data-testid="addRestaurantButton"]').click();
     cy.get('[data-testid="newRestaurantName"]').type(restaurantName);
     cy.get('[data-testid="saveNewRestaurantButton"]').click();
+    //confirm newRestaurantForm is not displayed
+    cy.get('[data-testid="newRestaurantForm"]').should("not.exist");
     cy.contains(restaurantName);
   });
 });
