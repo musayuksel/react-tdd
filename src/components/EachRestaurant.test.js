86fcd0f86b7d36b840750c26817b77e50e730b4b
Musa <79163385+musayuksel@users.noreply.github.com>
2022-07-25 14:47:24 +0100

    Update test

--- src/components/EachRestaurant.test.js
+++ src/components/EachRestaurant.test.js
@@ -1,8 +1,15 @@
 import { render } from "@testing-library/react";
 import EachRestaurant from "./EachRestaurant";
-import RestaurantList from "./RestaurantList";
 describe("<EachRestaurant />", () => {
   describe("Should add restaurant in the list", () => {
+    // it("should NOT ADD empty restaurant to the list", () => {
+    //   const { container } = render(<EachRestaurant restaurant={""} />);
+
+    //   const listItems = container.getElementsByClassName(
+    //     "restaurant-list-item"
+    //   );
+    //   expect(listItems.length).toBe(18);
+    // });
     it("should show the restaurant on the list", async () => {
       const { container, getAllByRole } = render(
         <EachRestaurant restaurant={"Sushi restaurant"} />
@@ -11,10 +18,6 @@ describe("<EachRestaurant />", () => {
       expect(
         listItemsContents[0].textContent.includes("Sushi restaurant")
       ).toBe(true);
-      //   const listItems = container.getElementsByClassName(
-      //     "restaurant-list-item"
-      //   );
-      //   expect(listItems.length).toBe(1);
     });
   });
 });
