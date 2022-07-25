55ebf1915172712478ce6ee3f4a49551d3128291
Musa <79163385+musayuksel@users.noreply.github.com>
2022-07-25 13:39:58 +0100

    Feat: Create each restaurant component

---
+++ src/components/EachRestaurant.jsx
@@ -0,0 +1,14 @@
+import React, { useId } from "react";
+
+export default function EachRestaurant({ restaurant, index }) {
+  console.log("first");
+  const id = useId();
+  return (
+    <li className="collection-item restaurant-list-item">
+      <p className="col s10">{restaurant}</p>
+      <button className="">
+        <i className="small material-icons col s2 ">delete_forever</i>
+      </button>
+    </li>
+  );
+}
