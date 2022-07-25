29abc8c39e6cbff991d4cdb41b77e4aa27829714
Musa <79163385+musayuksel@users.noreply.github.com>
2022-07-25 14:07:48 +0100

    Add new case for add rest test

--- cypress/e2e/adding_a_restaurant.cy.js
+++ cypress/e2e/adding_a_restaurant.cy.js
@@ -1,11 +1,17 @@
 describe("adding a restaurant", () => {
   it("display the restaurant in the list", () => {
     const restaurantName = "Sushi Place";
+    const restaurantName2 = "Sushi Place-2";
     cy.visit("http://localhost:3000");
     //confirm newRestaurantForm is not displayed
     cy.get('[data-testid="newRestaurantForm"]').should("not.exist");
     cy.get('[data-testid="addRestaurantButton"]').click();
     cy.get('[data-testid="newRestaurantName"]').type(restaurantName);
+    cy.get('[data-testid="saveNewRestaurantButton"]').click();
+    //confirm newRestaurantForm is not displayed
+    cy.get('[data-testid="newRestaurantForm"]').should("not.exist");
+    cy.get('[data-testid="addRestaurantButton"]').click();
+    cy.get('[data-testid="newRestaurantName"]').type(restaurantName2);
     cy.get('[data-testid="saveNewRestaurantButton"]').click();
     //confirm newRestaurantForm is not displayed
     cy.get('[data-testid="newRestaurantForm"]').should("not.exist");
