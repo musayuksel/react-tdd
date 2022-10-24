a679e8e4dbb570a8fe5b3752676e01b909a59146
Musa <79163385+musayuksel@users.noreply.github.com>
2022-10-24 10:06:19 +0100

    Add restaurant list and page

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import "materialize-css";
 import "materialize-css/dist/css/materialize.min.css";
 import EachRestaurant from "./EachRestaurant";
-export default function RestaurantList({ restaurants }) {
+export default function RestaurantList({ restaurants, setRestaurants }) {
   const restaurantsList = restaurants
     .filter((restaurant) => restaurant)
     .map((restaurant) => (
-      <EachRestaurant restaurant={restaurant} key={restaurant} />
+      <EachRestaurant
+        setRestaurants={setRestaurants}
+        restaurant={restaurant}
+        key={restaurant}
+      />
     ));
   return (
     <ul className="collection with-header">
--- src/components/RestaurantListPage.jsx
+++ src/components/RestaurantListPage.jsx
@@ -3,17 +3,35 @@ import NewRestaurantForm from "./NewRestaurantForm";
 import RestaurantList from "./RestaurantList";
 import "materialize-css";
 import "materialize-css/dist/css/materialize.min.css";
-
 export default function RestaurantListPage() {
-  const [restaurants, setRestaurants] = useState([]);
+  // LAZY LOADING
+  const [count, setCount] = React.useState(0);
+  const increment = async () => {
+    return await setTimeout(() => {
+      console.log(count);
+      setCount((prev) => prev + 1);
+    }, 1000);
+  };
+
+  function testDelete() {
+    // setTimeout(() => {
+    console.log("LAZY LOADING");
+    return [];
+    // }, 1000);
+  }
+  const [restaurants, setRestaurants] = useState(() => testDelete());
 
   function saveHandler(name) {
     setRestaurants((prevRestaurants) => [...prevRestaurants, name]);
   }
   return (
     <section>
+      <button onClick={increment}>TEST-{count}</button>
       <NewRestaurantForm saveHandler={saveHandler} />
-      <RestaurantList restaurants={restaurants} />
+      <RestaurantList
+        restaurants={restaurants}
+        setRestaurants={setRestaurants}
+      />
     </section>
   );
 }
