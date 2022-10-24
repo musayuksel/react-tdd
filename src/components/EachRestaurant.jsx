c19b30215c788113202643353a5494b7076691eb
Musa <79163385+musayuksel@users.noreply.github.com>
2022-10-24 10:06:02 +0100

    Add Each restaurant component

--- src/components/EachRestaurant.jsx
+++ src/components/EachRestaurant.jsx
@@ -1,13 +1,15 @@
-import React, { useId } from "react";
+import React from "react";
 
-export default function EachRestaurant({ restaurant, index }) {
-  console.log("first");
-  const id = useId();
-
+export default function EachRestaurant({ restaurant, setRestaurants }) {
+  function handleDelete() {
+    setRestaurants((prevRestaurants) =>
+      prevRestaurants.filter((eachRestaurant) => eachRestaurant !== restaurant)
+    );
+  }
   return (
     <li className="collection-item restaurant-list-item">
       <p className="col s10">{restaurant}</p>
-      <button className="delete-item">
+      <button onClick={handleDelete} className="delete-item">
         <i className="small material-icons col s2 ">delete_forever</i>
       </button>
     </li>
