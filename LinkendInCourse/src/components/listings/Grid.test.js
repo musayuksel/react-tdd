366315bf78a23a6c64b3ca1886365472c129a2fb
Musa <79163385+musayuksel@users.noreply.github.com>
2022-10-24 10:58:36 +0100

    Add format

--- LinkendInCourse/src/components/listings/Grid.test.js
+++ LinkendInCourse/src/components/listings/Grid.test.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import data from '../../data/courses.json'
+import data from '../../data/courses.json';
 import Grid from './Grid';
-import {
-    render,
-    screen,
-  } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 const numberItems = data.length;
 
-    it('should render 12 items', () => {
-        render(<Grid items={data} />);
-        const items = screen.getAllByRole('listitem');
-        expect(items).toHaveLength(numberItems);
-        
-    });
-
-
+it('should render 12 items', () => {
+  render(<Grid items={data} />);
+  const items = screen.getAllByRole('listitem');
+  expect(items).toHaveLength(numberItems);
+});
